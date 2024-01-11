1557e1125c51f6379633019c38b481d67e504309
oleeriksen <oe@easv.dk>
2024-01-11 08:47:31 +0100

    Initial commit

---
+++ indexer/App.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Shared;
+
+namespace Indexer
+{
+    public class App
+    {
+        public App()
+        {
+        }
+
+        public void Run()
+        {
+            Database db = new Database();
+            Crawler crawler = new Crawler(db);
+            
+
+            var root = new DirectoryInfo(Paths.FOLDER);
+
+            DateTime start = DateTime.Now;
+
+            crawler.IndexFilesIn(root, new List<string> { ".txt"});
+            
+
+            TimeSpan used = DateTime.Now - start;
+            Console.WriteLine("DONE! used " + used.TotalMilliseconds);
+
+            var all = db.GetAllWords();
+
+            foreach (var p in all)
+            {
+                Console.WriteLine("<" + p.Key + ", " + p.Value + ">");
+                break;
+            }
+
+
+        }
+
+
+    }
+}
