b06321c6c7903040f81fe7c337de3406b78a84c1
oleeriksen <oleeriksen1958@gmail.com>
2024-02-19 14:29:14 +0100

    done

---
+++ indexer/Term.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Indexer
+{
+    public class Term
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+        public int Count { get; set; }
+    }
+}
+
